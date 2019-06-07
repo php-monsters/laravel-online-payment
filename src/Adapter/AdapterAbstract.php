c43d76c3ce69b125752d88148dee442545674412
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2019-06-07 21:41:20 +0430

    fixed characters case in check paramaters

--- src/Adapter/AdapterAbstract.php
+++ src/Adapter/AdapterAbstract.php
@@ -181,9 +181,9 @@ abstract class AdapterAbstract
      */
     protected function checkRequiredParameters(array $parameters)
     {
-        $parameters = array_change_key_case($parameters,CASE_LOWER);
-
         foreach ($parameters as $parameter) {
+            $parameter = strtolower($parameter);
+
             if (!array_key_exists($parameter, $this->parameters) || trim($this->parameters[$parameter]) == "") {
                 throw new Exception("Parameters array must have a not null value for key: '$parameter'");
             }
