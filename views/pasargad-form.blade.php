4a09bf9c5747cca911bcecdb367c1996a81b1714
Aboozar Ghaffari <aboozar.ghf@gmail.com>
2020-12-07 19:32:18 +0330

    fixed Pasargad form problem with callback URL that included query string parameters

--- views/pasargad-form.blade.php
+++ views/pasargad-form.blade.php
@@ -1,61 +1,24 @@
-<html>
-    <body>
-        <script>
-        	var form = document.createElement("FORM");
-        	form.setAttribute("method", "POST");
-        	form.setAttribute("action", "{{$url}}");
-        	form.setAttribute("target", "_self");
+<form id="goto_pasargad_bank" class="form-horizontal goto-bank-form" method="POST" target="_self" action="{!! $url !!}">
+    <input type="hidden" name="token" value="{{$refId}}" />
+    <input type="hidden" name="invoiceNumber" value="{{$invoiceNumber}}" />
+    <input type="hidden" name="invoiceDate" value="{!! $invoiceDate !!}" />
+    <input type="hidden" name="amount" value="{{$amount}}" />
+    <input type="hidden" name="terminalCode" value="{{$terminalCode}}" />
+    <input type="hidden" name="merchantCode" value="{{$merchantCode}}" />
+    <input type="hidden" name="timeStamp" value="{!! $timeStamp !!}" />
+    <input type="hidden" name="action" value="{{$action}}" />
+    <input type="hidden" name="sign" value="{!! $sign !!}" />
+    <input type="hidden" name="redirectAddress" value="{!! $redirectUrl !!}" />
+    <div class="control-group">
+        <div class="controls">
+            <button type="submit" class="btn btn-success">{{$submitLabel}}</button>
+        </div>
+    </div>
+</form>
 
-            var invoiceNumber = document.createElement("input");
-			invoiceNumber.setAttribute("name", "invoiceNumber");
-			invoiceNumber.setAttribute("value", "{{$invoiceNumber}}");
-
-            form.appendChild(invoiceNumber);
-
-			var invoiceDate = document.createElement("input");
-			invoiceDate.setAttribute("name", "invoiceDate");
-			invoiceDate.setAttribute("value", "{{$invoiceDate}}");
-            form.appendChild(invoiceDate);
-
-			var amount = document.createElement("input");
-			amount.setAttribute("name", "amount");
-			amount.setAttribute("value", "{{$amount}}");
-            form.appendChild(amount);
-
-			var terminalCode = document.createElement("input");
-			terminalCode.setAttribute("name", "terminalCode");
-			terminalCode.setAttribute("value", "{{$terminalCode}}");
-            form.appendChild(terminalCode);
-
-			var merchantCode = document.createElement("input");
-			merchantCode.setAttribute("name", "merchantCode");
-			merchantCode.setAttribute("value", "{{$merchantCode}}");
-            form.appendChild(merchantCode);
-
-			var timeStamp = document.createElement("input");
-			timeStamp.setAttribute("name", "timeStamp");
-			timeStamp.setAttribute("value", "{{$timeStamp}}");
-            form.appendChild(timeStamp);
-
-			var action = document.createElement("input");
-			action.setAttribute("name", "action");
-			action.setAttribute("value", "{{$action}}");
-            form.appendChild(action);
-
-			var sign = document.createElement("input");
-			sign.setAttribute("name", "sign");
-			sign.setAttribute("value", "{{$sign}}");
-            form.appendChild(sign);
-
-			var redirectAddress = document.createElement("input");
-			redirectAddress.setAttribute("name", "redirectAddress");
-			redirectAddress.setAttribute("value", "{{$redirectUrl}}");
-            form.appendChild(redirectAddress);
-
-			document.body.appendChild(form);
-            form.submit();
-        	//document.write(form.outerHTML());
-        	document.body.removeChild(form);
-        </script>
-    </body>
-</html>
+@if($autoSubmit === true)
+    <script type="text/javascript">
+	var f=document.getElementById('goto_pasargad_bank');
+    f.submit();
+</script>
+@endif
